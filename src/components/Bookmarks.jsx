6304798058244194490a84ddf7dd4e7d33fa6737
serch3 <alwovive@gmail.com>
2025-06-03 12:02:52 -0500

    small design bug fixes

--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -51,7 +51,7 @@ const Bookmarks = () => {
               <div
                 ref={provided.innerRef}
                 {...provided.droppableProps}
-                className={`mb-5 p-2 rounded-md ${
+                className={`mb-5 p-1.5 rounded-md ${
                   snapshot.isDraggingOver ? 'bg-gray-700/30' : ''
                 }`}
               >
